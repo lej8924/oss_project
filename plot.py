f03e78a2b9e0f16020c228442666518e98653199
Yong Uk <kimyu9827@naver.com>
2021-12-19 06:36:40 +0900

    add plot.py

---
+++ plot.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+#시각화
+data1 = pd.read_csv('final.csv')
+data1 = data1.drop('Unnamed: 0',axis=1)
+
+star = data1['Score']
+numstar = data1['Numberofscore']
+total = data1['Totalscore']
+
+xs = np.array(star)
+ys = np.array(numstar)
+zs = np.array(total)
+fig = plt.figure(figsize=(6, 6))
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(xs, ys, zs, marker='o', s=1, cmap='Greens')
+plt.show()
+
+plt.figure()
+plt.scatter(ys,zs,marker='+')
+plt.grid(True)
+plt.show()
