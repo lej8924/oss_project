375a8fd9ed430b615fb9d2a1326f1dd195cad1f3
Yong Uk <kimyu9827@naver.com>
2021-12-21 01:13:58 +0900

    sketch file update

--- discordbot/sketch.py
+++ discordbot/sketch.py
@@ -1,5 +1,4 @@
 import asyncio, discord
-from discord import embeds
 from discord.embeds import Embed
 from discord.ext import commands
 from discord_buttons_plugin import *
@@ -15,6 +14,7 @@ data = pd.read_csv('discordbot/final.csv')
 data = data.drop('Unnamed: 0',axis=1)
 
 userdata = [None, None, 1]
+
 def finddata(temp):
         i = random.randint(0,len(temp))
         name = str(temp['Name'][i:i+1].values[0])
@@ -40,10 +40,10 @@ async def select(ctx):
     await selectboxTesting(ctx) 
 
 async def selectboxTesting(ctx):
-    await ctx.send(content="어떤 기준으로 맛집을 찾을까요?",
+    await ctx.send(content="맛집을 찾는 기준이 있으신가요?",
                    components=[
                     Select(
-                    placeholder="기준을 선택해 주세요",
+                    placeholder="본인의 기준을 선택해 주세요!",
                     options = [
                         SelectOption(
                         label="지역",
@@ -59,14 +59,20 @@ async def selectboxTesting(ctx):
                         label="아님 새로운 음식 도전!!",
                         value='3',
                         description = "난 딴 거 필요없이 새로운 곳 갈래!",
+                        emoji="😄"),
+                        SelectOption(
+                        label="바로 추천 해줘!!",
+                        value='4',
+                        description = "우리 지금 만나! 당장 만나!",
                         emoji="😄")
                         ]
                     )
                 ]
             )
-    e1 = discord.Embed(title="!location", description="이 키워드를 채팅에 적어보세요!!", color=0xffd032)
-    e2 = discord.Embed(title="!type", description="이 키워드를 채팅에 적어보세요!!", color=0xffd032)
-    e3 = discord.Embed(title="!challenge", description="이 키워드를 채팅에 적어보세요!!", color=0xffd032)
+    e1 = discord.Embed(title="!location", description="찾고싶은 지역이 있다면\n이 키워드를 채팅에 적어보세요!!", color=0xc63c1c)
+    e2 = discord.Embed(title="!type", description="먹고싶은 음식이 있다면\n이 키워드를 채팅에 적어보세요!!", color=0xc63c1c)
+    e3 = discord.Embed(title="!challenge", description="모험을 좋아한다면\n이 키워드를 채팅에 적어보세요!!", color=0xc63c1c)
+    e4 = discord.Embed(title="!find",description = "아무렴 어때!\n이 키워드를 입력하고 빨리 추천받으세요!!",color=0xc63c1c)
 
     while True:
         event = await bot.wait_for("select_option", check=None)
@@ -79,6 +85,8 @@ async def selectboxTesting(ctx):
             await event.send(ephemeral=True,embed=e2)
         elif label == "3":
             await event.send(ephemeral=False,embed=e3 )
+        elif label =='4':
+            await event.send(ephemeral=False,embed=e4)
 ######select문 끝######
 
 ######지역 버튼######
@@ -114,28 +122,28 @@ async def location(ctx):
 @buttons.click
 async def gong(ctx):
     userdata[0] = 'A'
-    await ctx.reply("공릉동 맛집을 찾아드리겠습니다.")
+    await ctx.reply("공릉동 맛집을 찾아드리겠습니다.  다른 기준도 선택해 보세요!\n!find 명령어를 입력하면 음식점을 추천합니다.")
 
 @buttons.click
 async def wol(ctx):
     userdata[0] = 'B'
-    await ctx.reply("월계동 맛집을 찾아드리겠습니다.")
+    await ctx.reply("월계동 맛집을 찾아드리겠습니다. 다른 기준도 선택해 보세요!\n!find 명령어를 입력하면 음식점을 추천합니다.")
 
 @buttons.click
 async def sangye(ctx):
     userdata[0] = 'C'
-    await ctx.reply("상계동 맛집을 찾아드리겠습니다")
+    await ctx.reply("상계동 맛집을 찾아드리겠습니다. 다른 기준도 선택해 보세요!\n!find 명령어를 입력하면 음식점을 추천합니다.")
     
 @buttons.click
 async def jungye(ctx):
     userdata[0] = 'D'
     print(userdata)
-    await ctx.reply("중계동 맛집을 찾아드리겠습니다")
+    await ctx.reply("중계동 맛집을 찾아드리겠습니다. 다른 기준도 선택해 보세요!\n!find 명령어를 입력하면 음식점을 추천합니다.")
     
 @buttons.click
 async def hagye(ctx):
     userdata[0] = 'E'
-    await ctx.reply("하계동 맛집을 찾아드리겠습니다.")
+    await ctx.reply("하계동 맛집을 찾아드리겠습니다. 다른 기준도 선택해 보세요!\n!find 명령어를 입력하면 음식점을 추천합니다.")
 ######지역버튼 끝######
 
 ######음식 버튼######
@@ -171,28 +179,28 @@ async def type(ctx):
 @buttons.click
 async def kor(ctx):
     userdata[1] = 0
-    await ctx.reply("한식 맛집을 찾아드릴게요.")
+    await ctx.reply("한식 맛집을 찾아드릴게요. 다른 기준도 선택해 보세요!\n !find 명령어를 입력하면 음식점을 추천합니다.")
 
 @buttons.click
 async def chi(ctx):
     userdata[1] = 1
-    await ctx.reply("중식 맛집을 찾아드릴게요.")
+    await ctx.reply("중식 맛집을 찾아드릴게요. 다른 기준도 선택해 보세요!\n !find 명령어를 입력하면 음식점을 추천합니다.")
 
 @buttons.click
 async def jap(ctx):
     userdata[1] = 2
     print(userdata)
-    await ctx.reply("일식 맛집을 찾아드릴게요")
+    await ctx.reply("일식 맛집을 찾아드릴게요. 다른 기준도 선택해 보세요!\n !find 명령어를 입력하면 음식점을 추천합니다.")
     
 @buttons.click
 async def west(ctx):
     userdata[1] = 3
-    await ctx.reply("양식 맛집을 찾아드릴게요")
+    await ctx.reply("양식 맛집을 찾아드릴게요. 다른 기준도 선택해 보세요!\n !find 명령어를 입력하면 음식점을 추천합니다.")
     
 @buttons.click
 async def cafe(ctx):
     userdata[1] = 4
-    await ctx.reply("카페를 찾아드릴게요.")
+    await ctx.reply("카페를 찾아드릴게요. 다른 기준도 선택해 보세요!\n !find 명령어를 입력하면 음식점을 추천합니다.")
 ######음식버튼 끝######
 
 ######도전 버튼######
@@ -226,14 +234,14 @@ async def nch(ctx):
 ######도전버튼 끝######
 
 @bot.command()
-async def fin(ctx):
+async def find(ctx):
     print(userdata)
-    if userdata[1] == None:
+    if (userdata[0]!=None) and (userdata[1] == None):
         temp = data.loc[(data['Dong']== userdata[0]) & (data['Challenge']== userdata[2])]
         embed = finddata(temp)
         await ctx.send(embed = embed)
 
-    if userdata[0] == None:
+    if (userdata[0] == None) and (userdata[1] !=None):
         temp = data.loc[(data['Typenum']==userdata[1]) & (data['Challenge']== userdata[2])]
         embed = finddata(temp)
         await ctx.send(embed = embed)
@@ -242,5 +250,12 @@ async def fin(ctx):
         temp = data.loc[(data['Dong']== userdata[0]) & (data['Typenum']==userdata[1]) & (data['Challenge']== userdata[2])]
         embed = finddata(temp)
         await ctx.send(embed = embed)
+    
+    if (userdata[0]==None) and (userdata[1] ==None):
+        location = random.choice(['A', 'B', 'C', 'D','E'])
+        type = random.choice([0, 1, 2, 3, 4])
+        temp = temp = data.loc[(data['Dong']== location) & (data['Typenum']== type) & (data['Challenge']== userdata[2])]
+        embed = finddata(temp)
+        await ctx.send(embed = embed)
 
-bot.run('OTE1Mjk1NTE5MDEyNzE2NTQ1.YaZhYQ.KyWergE9I2hzEmB9JBOOdAzVgEA') #토큰
+bot.run('OTE1Mjk1NTE5MDEyNzE2NTQ1.YaZhYQ.XwZtSU4r4vkEB2SlHTxIgGehN98') #토큰
